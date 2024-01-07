adff3397505fd8af6d91e73b9c6285fbc286cbc2
carlyleliu <yyliushuai@gmail.com>
2024-01-07 14:24:51 +0800

    custom: config basic nvim

--- lua/config/basic.lua
+++ lua/config/basic.lua
@@ -1,8 +1,12 @@
 -- utf-8
 --
-
-local opt = vim.opt
-
+-- vim.o 获取或设置常规设置
+-- vim.wo 获取或设置窗口范围的选项
+-- vim.bo 获取或设置缓冲区范围的选项
+-- vim.g 获取或设置全局变量
+-- vim.env 获取或设置环境变量
+-- vim.opt 可以设置全局、窗口和缓冲区设置
+--
 -- 显示左侧图标指示列
 vim.wo.signcolumn = "yes"
 
@@ -10,29 +14,26 @@ vim.wo.signcolumn = "yes"
 vim.wo.colorcolumn = "120"
 
 -- 缩进2个空格等于一个Tab
-vim.o.tabstop = 4
-vim.bo.tabstop = 4
-vim.o.softtabstop = 4
-vim.o.shiftround = true
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftround = true
 
 -- >> << 时移动长度
-vim.o.shiftwidth = 4
-vim.bo.shiftwidth = 4
+vim.opt.shiftwidth = 4
 
 -- 空格替代tab
-vim.o.expandtab = true
-vim.bo.expandtab = true
+vim.opt.expandtab = true
 
 -- 搜索大小写不敏感，除非包含大写
-vim.o.ignorecase = true
-vim.o.smartcase = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
 
 -- 不可见字符的显示，这里只把空格显示为一个点
-vim.o.list = true
-vim.o.listchars = "tab:>·,trail:-,space:·"
+vim.opt.list = true
+vim.opt.listchars = "tab:<-,trail:·,space:·"
 
 -- 设置系统剪切板
-opt.clipboard = "unnamedplus" -- Sync with system clipboard
+vim.opt.clipboard = "unnamedplus" -- Sync with system clipboard
 
 -- 不自动格式化文件
 vim.g.autoformat = false
